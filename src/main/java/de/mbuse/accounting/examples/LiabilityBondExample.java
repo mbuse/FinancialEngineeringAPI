89f8b0a3e217b76358de23fd5b585bcff61112b5
mbuse <matthias.buse@coremedia.com>
2013-10-31 16:33:43 +0100

    adding examples for discount bonds

--- src/main/java/de/mbuse/accounting/examples/LiabilityBondExample.java
+++ src/main/java/de/mbuse/accounting/examples/LiabilityBondExample.java
@@ -14,9 +14,9 @@ public class LiabilityBondExample {
   
   public static void main(String... args) throws Exception {
     TAccount cash = new TAccount("Cash", TAccount.Type.ASSET);
-    Bond bond = new Bond("KP Inc.", 10000, 0.05, 0.04, 3, Bond.CouponPayment.SEMIANUALLY);
+    Bond bond = new Bond("Premium Bond", 10000, 0.05, 0.04, 3, Bond.CouponPayment.SEMIANUALLY);
     
-    System.out.println("Bond price: " + FMT.formatMoney(bond.getInitialPrice()));
+    System.out.println("=== Premium Bond Example ===\n");
     
     Calendar cal = Calendar.getInstance();
     cal.setTime(FMT.getDateFormat().parse("01.01.2010"));
@@ -34,8 +34,33 @@ public class LiabilityBondExample {
     FMT.print(bond.getInterestExpense());
     
     
-    // Bond Retirement...
-    bond = new Bond("Retirement Inc.", 10000, 0.05, 0.04, 3, Bond.CouponPayment.SEMIANUALLY);
+    System.out.println("\n\n=== Discount Bond Example ===\n");
+    
+    cash = new TAccount("Cash", TAccount.Type.ASSET);
+    bond = new Bond("Discount Bond", 10000, 0.05, 0.06, 3, Bond.CouponPayment.SEMIANUALLY);
+    
+    System.out.println("Bond price: " + FMT.formatMoney(bond.getInitialPrice()));
+    
+    cal = Calendar.getInstance();
+    cal.setTime(FMT.getDateFormat().parse("01.01.2010"));
+    bond.issue(cal.getTime(), cash);
+    
+    for (int p=0; p<6; p++) {
+      cal.add(Calendar.MONTH, 6);
+      bond.payCoupon(cal.getTime(), cash);
+    }
+    bond.mature(cal.getTime(), cash);
+    
+    FMT.print(bond.getJournal());
+    FMT.print(cash);
+    FMT.print(bond.getBondsPayable());
+    FMT.print(bond.getInterestExpense());
+    
+    
+    System.out.println("\n\n=== Bond Retirment Example ===\n");
+    
+    cash = new TAccount("Cash", TAccount.Type.ASSET);
+    bond = new Bond("Retirement", 10000, 0.05, 0.04, 3, Bond.CouponPayment.SEMIANUALLY);
     cal = Calendar.getInstance();
     cal.setTime(FMT.getDateFormat().parse("01.01.2010"));
     bond.issue(cal.getTime(), cash);
